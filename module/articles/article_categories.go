13b1152b7423248ff068ae072f49120fa5cfd4cd
Daaaaaav <144467455+Daaaaaav@users.noreply.github.com>
2024-10-14 23:18:02 +0700

    Update article_categories.go

--- module/articles/article_categories.go
+++ module/articles/article_categories.go
@@ -1,7 +1,8 @@
 package articles
 
 type ArticleCategories struct {
-	ID   uint   `gorm:"not null;primaryKey" json:"id"`
-	Name string `gorm:"not null;unique" json:"name"`
-	Slug string `gorm:"not null;unique" json:"slug"`
+	ID       uint       `gorm:"not null;primaryKey" json:"id"`
+	Name     string     `gorm:"not null;unique" json:"name"`
+	Slug     string     `gorm:"not null;unique" json:"slug"`
+	Articles []Articles `gorm:"foreignKey:CategoryID"`
 }
