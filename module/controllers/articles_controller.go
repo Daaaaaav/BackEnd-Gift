94f6744e43d4ae47eb41527a748f595cbc91988d
Daaaaaav <144467455+Daaaaaav@users.noreply.github.com>
2024-10-14 23:19:35 +0700

    Update articles_controller.go

--- module/controllers/articles_controller.go
+++ module/controllers/articles_controller.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/microcosm-cc/bluemonday"
 	"gorm.io/gorm"
 )
 
@@ -23,70 +24,65 @@ func (ac *ArticlesController) AddArticle(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	p := bluemonday.UGCPolicy()
+	article.Title = p.Sanitize(article.Title)
+	article.Content = p.Sanitize(article.Content)
+	article.Thumbnail = p.Sanitize(article.Thumbnail)
+	article.Slug = p.Sanitize(article.Slug)
 	if err := ac.DB.Create(&article).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create article!"})
 		return
 	}
-
 	c.JSON(http.StatusOK, article)
 }
 
 func (ac *ArticlesController) GetArticle(c *gin.Context) {
 	id := c.Param("id")
-
 	var article articles.Articles
 	if err := ac.DB.Preload("Category").First(&article, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Article not found!"})
 		return
 	}
-
 	c.JSON(http.StatusOK, article)
 }
 
-func (ac *ArticlesController) GetArticles(c *gin.Context) {
-	pageStr := c.Query("page")
-	pageSizeStr := c.Query("page_size")
-
-	page, _ := strconv.Atoi(pageStr)
-	if page < 1 {
-		page = 1
-	}
-	pageSize, _ := strconv.Atoi(pageSizeStr)
-	if pageSize < 1 {
-		pageSize = 10
-	}
-
-	offset := (page - 1) * pageSize
-
+func (ac *ArticlesController) GetArticles(ctx *gin.Context) {
 	var articles []articles.Articles
-	if err := ac.DB.Preload("Category").Order("created_at desc").Offset(offset).Limit(pageSize).Find(&articles).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch!"})
+	keyword := ctx.Query("keyword")
+	categoryID := ctx.Query("category_id")
+	query := ac.DB.Preload("Category").Order("created_at desc")
+	if categoryID != "" {
+		query = query.Where("category_id = ?", categoryID)
+	}
+	if keyword != "" {
+		query = query.Where("title ILIKE ?", "%"+keyword+"%")
+	}
+	err := query.Find(&articles).Error
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch articles!"})
 		return
 	}
-
-	c.JSON(http.StatusOK, articles)
+	ctx.JSON(http.StatusOK, articles)
 }
 
 func (ac *ArticlesController) UpdateArticle(c *gin.Context) {
 	id := c.Param("id")
 	var article articles.Articles
-
 	if err := ac.DB.First(&article, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Article not found!"})
 		return
 	}
-
 	if err := c.ShouldBindJSON(&article); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	p := bluemonday.UGCPolicy()
+	article.Title = p.Sanitize(article.Title)
+	article.Content = p.Sanitize(article.Content)
 	if err := ac.DB.Save(&article).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update article!"})
 		return
 	}
-
 	c.JSON(http.StatusOK, article)
 }
 
@@ -97,11 +93,9 @@ func (ac *ArticlesController) DeleteArticle(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID, try again!"})
 		return
 	}
-
 	if err := ac.DB.Delete(&articles.Articles{}, id).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete article!"})
 		return
 	}
-
 	c.JSON(http.StatusOK, gin.H{"message": "Article successful to be deleted!"})
 }
